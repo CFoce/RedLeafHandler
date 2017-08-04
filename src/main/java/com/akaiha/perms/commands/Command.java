982935771fd1150dfeb17b038efa31dc6ef18b11
CFoce <cfoce.dev@gmail.com>
2017-08-04 19:49:12 -0400

    update

--- src/main/java/com/akaiha/perms/commands/Command.java
+++ src/main/java/com/akaiha/perms/commands/Command.java
@@ -19,13 +19,29 @@ public class Command implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-		Player p = (Player) sender;
-		PermissionAttachment pa = plugin.perms.get(p.getUniqueId());
-		String str = "";
-		for (Map.Entry<String, Boolean> entry : pa.getPermissions().entrySet()) {
-			str += entry.getKey() + " = " + entry.getValue() + ", ";
+		if (args.length > 0 && sender.hasPermission("perms.perms.others")) {
+			@SuppressWarnings("deprecation")
+			Player p = plugin.getServer().getPlayer(args[0]);
+			if (p == null) {
+				return false;
+			}
+			PermissionAttachment pa = plugin.perms.get(p.getUniqueId());
+			String str = "";
+			for (Map.Entry<String, Boolean> entry : pa.getPermissions().entrySet()) {
+				str += entry.getKey() + " = " + entry.getValue() + ", ";
+			}
+			sender.sendMessage(str);
+			return false;
 		}
-		p.sendMessage(str);
+		if (sender instanceof Player && sender.hasPermission("perms.perms")) {
+			Player p = (Player) sender;
+			PermissionAttachment pa = plugin.perms.get(p.getUniqueId());
+			String str = "";
+			for (Map.Entry<String, Boolean> entry : pa.getPermissions().entrySet()) {
+				str += entry.getKey() + " = " + entry.getValue() + ", ";
+			}
+			p.sendMessage(str);
+		}
 		return false;
 	}
 
